125424355e2c8c22cb699abf082cf1985ccfbb1b
Lakshay Kamboj <lakshay.kamboj31@gmail.com>
2025-04-23 12:23:26 +0000

    container config done

--- 01_basics/dataTypes-summary.js
+++ 01_basics/dataTypes-summary.js
@@ -34,3 +34,28 @@ console.log("hello world");
 
 
 console.log(typeof anotherId);
+
+
+// ********************************************************************
+
+// stack(primitive), Heap(Non-primitive) 
+
+let myYoutubeName = "lakshaykamboj.com"
+let anotherName = myYoutubeName
+anotherName = "chai or code"
+console.log(myYoutubeName);
+console.log(anotherName);
+
+
+let userOne = {
+    email: "lakshay.kamnboj2.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+userTwo.email = "lakshaykamboj@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
