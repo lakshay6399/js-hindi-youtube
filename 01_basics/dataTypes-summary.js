49be7a07dc6d9f5c849638d33011c77594e9cc09
Lakshay Kamboj <lakshay.kamboj31@gmail.com>
2025-04-23 12:11:17 +0000

    container config done

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,36 @@
+// primitive dataTypes
+
+// there are 7 types : String, Number, Boolean, null, undefined, BigInt, Symbol
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+
+const ourSide = null
+
+let userEmail;
+const id = Symbol('1234')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+// const bigNumber = 223423456756n 
+
+
+// Non - Primitive / reference
+
+// Array, Objects, Functions
+
+const heroes = ["shaktiman", "naagraj", "doga"]
+let myObj= {
+    name: "lakshay",
+    age: 24,
+}
+
+
+const myFunction = function(){
+console.log("hello world");
+}
+
+
+console.log(typeof anotherId);
