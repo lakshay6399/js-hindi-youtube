d4e14c7d62a25bc1e7b8c7b17360f31712f296ec
Lakshay Kamboj <lakshay.kamboj31@gmail.com>
2025-04-24 07:27:13 +0000

    container config done

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,44 @@
+const name = " lakshay"
+const repoCount = 50
+// console.log(name + repoCount + "value");
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+const gameName = new String('laksh-kamboj')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('h'));
+
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-3, 2)
+// console.log(anotherString);
+
+const newStringOne = " laksh "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://www.google.09com"
+console.log(url.replace('09', ''));
+
+console.log(url.includes('google'));
+console.log(url.includes('laksh'));
+
+console.log(gameName.split('-'));
+
+
+
+
+
+
+
+
+
+
+
