1278a9422117db61aec403294545bcc07fa7b1aa
Lakshay Kamboj <lakshay.kamboj31@gmail.com>
2025-04-25 11:29:24 +0000

    container config done

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,59 @@
+
+// var c = 300
+let a = 300
+
+if(true){
+    let a = 10
+    const b = 20
+    // var c = 30
+//   console.log(a);
+  
+}
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const username = "lakshay"
+    function two(){
+        const website = "Youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+     two()
+    
+}
+// one()
+
+if (true) {
+    const username = "lakshay"
+    if (username === "lakshay") {
+        const website = " Youtube"
+        // console.log(username + website);
+        
+        
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+
+
+// ***************** interesting **********************
+
+function addone(num){
+    return num + 1
+    
+}
+console.log(addone(5))
+
+const addTwo = function (num) {
+    return num + 2
+    
+}
+addTwo(5)
+
