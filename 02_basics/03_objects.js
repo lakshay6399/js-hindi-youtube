b747df007ebeb87130faf87e7570de1b5d6403ae
Lakshay Kamboj <lakshay.kamboj31@gmail.com>
2025-04-25 03:16:43 +0000

    container config done

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,38 @@
+// singleton 
+
+// object literals
+
+const mySym = Symbol("key1")
+
+const JsUser = {
+    name: "laksh",
+    "full name": "laksh kamboj",
+     [mySym]: "mykey1",
+    age: 24,
+    location: "new delhi",
+    email: "lakshay.kamboj31@gmail.com",
+    isLoggedIn : false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "lakshay@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "lakshay.kamboj@.com"
+// console.log(JsUser);
+
+
+
+JsUser.greeting = function(){
+    console.log("hello js user");
+    
+}
+JsUser.greetingTwo = function(){
+    console.log(`hello js user, ${this.name}`);
+    
+}
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
