9019802bbcd590321632468c2773ce5e28d75371
Lakshay Kamboj <lakshay.kamboj31@gmail.com>
2025-04-26 12:15:45 +0000

    container config done

---
+++ 04_control_flow/if_and_else.js
@@ -0,0 +1,61 @@
+// if 
+// const isUserloggedIn = true
+// const temperature = 40
+
+// if (temperature === 40 ) {
+//     console.log("less than fifty");
+       
+// } else {
+//     console.log("greater than fifty");
+// }
+
+
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+
+const score = 200
+
+if (score > 100) {
+    const power = "fly"
+    // console.log(`user power: ${power}`);
+     
+}
+// console.log(`user power: ${power}`);
+
+
+
+const balance = 1000
+
+// if (balance > 500) console.log("test"), console.log("test2"); yeh kbhi mt krna
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// }else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance <900) {
+//     console.log("less than 900");
+    
+// }else {
+//  console.log("less than 1200");
+    
+// }
+
+const isUserloggedIn = true
+const debitCard = true 
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+
+if (isUserloggedIn && debitCard ) {
+    console.log("allow to courses");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("userloggedIn");
+    
+}
+
+
+
